01be059b6e5d6d21fe63d22f622f3816522abe4e
keshav kumar jha <iamkeshavjha14@gmail.com>
2025-11-27 04:18:11 +0000

    Now make a file name index.html o root or uss html file ko aise rakhna k

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,7 +2,7 @@ export default function GamePage() {
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src="/game.html"
+        src="/index.html"
         title="Game"
         style={{ border: 'none', width: '100%', height: '100%' }}
       />
